07dec5d5b0c8698c8f0ad764100582e1e31358b2
thesamarthrai <raisamarth19@gmail.com>
2024-03-17 04:44:28 +0530

    Day19

---
+++ Day19_2485.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int store =-1;
+        int count1 =0;
+        for(int i=1;i<=n;i++){
+            count1= count1+i;
+            int count2= 0;
+            for(int j=i;j<=n;j++){
+                count2=count2+j;
+            }
+            if(count1 == count2){
+                store =i;
+                break;
+                }
+            
+        }
+        if(store == -1){
+            return -1;
+        }else{
+            return store;
+        }
+    }
+};
